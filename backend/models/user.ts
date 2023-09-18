44c8e4a6af79c32b95e66034954e1bc448c9e21e
siiiido <discegaudere24.7@gmail.com>
2023-09-18 20:59:07 +0900

    set types User model

--- backend/models/user.ts
+++ backend/models/user.ts
@@ -1,14 +1,25 @@
 import mongoose from 'mongoose'
 import uniqueValidator from 'mongoose-unique-validator'
 
-const userSchema = new mongoose.Schema({
+interface IUser {
+  name: string
+  email: string
+  password: string
+  image: string
+  places: mongoose.Types.ObjectId
+}
+
+const userSchema = new mongoose.Schema<IUser>({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true, minlength: 6 },
   image: { type: String, required: true },
-  places: { type: mongoose.Types.ObjectId, required: true, ref: 'Place' },
+  places: [{ type: mongoose.Types.ObjectId, required: true, ref: 'Place' }],
+  //places: { type: mongoose.Types.ObjectId, required: true, ref: 'Place' }
 })
 
 userSchema.plugin(uniqueValidator)
 
-export default userSchema
+const User = mongoose.model<IUser>('User', userSchema)
+
+export default User
